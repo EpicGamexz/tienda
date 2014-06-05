69840a7186f549e126cc460e7d6ffbd36e45f480
EpicGamexz <epicgamexz@gmail.com>
2014-06-05 17:32:07 -0500

    upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mitienda::Application.config.secret_key_base = '875f9a8aa1748719a2b5360e27752b4c58ac9183689af13d451bc79f9b1b7f932326ff12ccb8a4b4c2b0fec42ee2b68fa19dbd65583c9964ebe1f6367aa33a5a'
